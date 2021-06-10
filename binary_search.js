28a995c9c667b8ac77c80a2525b3078d3c479362
JulieGumerman <julie.gumerman@gmail.com>
2021-06-09 20:55:44 -0600

    fixed binary search

--- binary_search.js
+++ binary_search.js
@@ -4,21 +4,17 @@ function binary_search (list, item) {
 	console.log("high", high)
 
 	while (low <= high) {
-		let mid = low + high
-		console.log("mid", mid)
+		let mid = Math.floor((low + high) /2)
 		let guess = list[mid]
-		console.log("guess", guess)
 
-		if (guess == item) {
-			console.log("found it!!!")
+
+		if (guess === item) {
+			console.log("found it!!! Guess:" + guess + " Index:" + mid)
 			return item
-		}
-		if (guess < item){
-			console.log("guess", guess)
-			low = guess + 1
-		} else {
-			console.log("guess", guess)
-			high = guess -1
+		} else if (guess < item){
+			low = mid + 1
+		} else if (guess > item){
+			high = mid -1
 		}
 		//return "Item not found"
 	}
